c0f02aaf9161edd493af8f1ca2b98111d4e2171b
infoomics <sbx_94@hotmail.com>
2023-03-02 19:00:40 +0000

    mirror name changes from star-schema.sql

--- star-schema-queries.sql
+++ star-schema-queries.sql
@@ -8,7 +8,7 @@ LIMIT 1;
 
 WITH events_worked_per_model AS (
     SELECT model_name, COUNT(model_id) AS events_worked
-    FROM fact_revenue
+    FROM fact_revenues
     JOIN dim_models USING (model_id)
     GROUP BY model_name, model_id
 ),
@@ -18,11 +18,13 @@ max_events_worked AS (
 )
 SELECT model_name
 FROM events_worked_per_model
-WHERE events_worked = (SELECT * FROM max_events_worked);
+WHERE events_worked = (
+    SELECT * FROM max_events_worked
+);
 
 WITH events_per_quarter AS (
     SELECT quarter, COUNT(quarter) AS events_count
-    FROM fact_revenue
+    FROM fact_revenues
     JOIN dim_dates USING (date_id)
     GROUP BY quarter
 ),
@@ -32,7 +34,9 @@ max_events_count AS (
 )
 SELECT quarter
 FROM events_per_quarter
-WHERE events_count = (SELECT * FROM max_events_count);
+WHERE events_count = (
+    SELECT * FROM max_events_count
+);
 
 -- SELECT agent_name FROM fact_rating JOIN agents ON fact_rating.agent_id = agents.agent_id GROUP BY agents.agent_id ORDER BY AVG(fact_rating.rating) ASC LIMIT 1;
 
