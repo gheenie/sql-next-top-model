cf1bd7ad4416a25f252cbf4a66112a83d2f694a9
infoomics <sbx_94@hotmail.com>
2023-03-02 18:36:23 +0000

    update file names; add lines for star schema

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,6 @@
-psql -f 1nf.sql > nf1.txt
-psql -f 2nf.sql > nf2.txt
-psql -f 3nf.sql > nf3.txt
-psql -f 3nf_queries.sql > nf3_queries.txt
+psql -f 1nf.sql > 1nf.txt
+psql -f 2nf.sql > 2nf.txt
+psql -f 3nf.sql > 3nf.txt
+psql -f 3nf-queries.sql > 3nf-queries.txt
+psql -f star-schema.sql > star-schema.txt
+psql -f star-schema-queries.sql > star-schema-queries.txt
